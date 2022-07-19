ce17a2d65171592759a538c4ca61ba4702c60fa2
Gjum <code.gjum@gmail.com>
2022-07-19 15:38:36 +0200

    implement PlayerNoMovement

---
+++ src/rosegold/packets/serverbound/player_no_movement.cr
@@ -0,0 +1,16 @@
+require "./packet"
+
+class Rosegold::Serverbound::PlayerNoMovement < Rosegold::Serverbound::Packet
+  PACKET_ID = 0x14_u8
+
+  property on_ground : Bool
+
+  def initialize(@on_ground); end
+
+  def to_packet : Minecraft::IO
+    Minecraft::IO::Memory.new.tap do |buffer|
+      buffer.write PACKET_ID
+      buffer.write on_ground
+    end
+  end
+end
