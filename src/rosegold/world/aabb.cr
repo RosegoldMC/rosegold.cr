feab2544cf1125540a183c937de1d6ccfab9581a
Gjum <code.gjum@gmail.com>
2022-07-19 04:03:41 +0200

    fix inheritance in Vec3f/d and AABBf/d

--- src/rosegold/world/aabb.cr
+++ src/rosegold/world/aabb.cr
@@ -1,8 +1,60 @@
-# axis aligned bounding box
-struct Rosegold::AABB
-  property \
-    x_min : Float32, y_min : Float32, z_min : Float32,
-    x_max : Float32, y_max : Float32, z_max : Float32
+require "./vec3"
 
-  def initialize(@x_min, @y_min, @z_min, @x_max, @y_max, @z_max); end
+# axis aligned bounding box
+module Rosegold::AABB(T)
+  def +(vec : Vec3(T)) : self
+    self.class.new min + vec, max + vec
+  end
+
+  def &+(vec : Vec3(T)) : self
+    self.class.new min + vec, max + vec
+  end
+
+  def grow(vec : Vec3(T)) : self
+    self.class.new min - vec, max + vec
+  end
+
+  def grow(dx : T, dy : T, dz : T) : self
+    self.class.new min.minus(dx, dy, dz), max.plus(dx, dy, dz)
+  end
+
+  def [](i) : Vec3(T)
+    {min, max}[i]
+  end
+end
+
+struct Rosegold::AABBf
+  include Rosegold::AABB(Float32)
+
+  getter min : Vec3f, max : Vec3f
+
+  def initialize(@min : Vec3f, @max : Vec3f); end
+
+  def self.new(
+    min_x : Float32, min_y : Float32, min_z : Float32,
+    max_x : Float32, max_y : Float32, max_z : Float32
+  )
+    self.new(Vec3f.new(min_x, min_y, min_z), Vec3f.new(max_x, max_y, max_z))
+  end
+
+  # additional methods that "upgrade" to AABBd
+
+  def +(vec : Vec3d) : AABBd
+    AABBd.new min + vec, max + vec
+  end
+end
+
+struct Rosegold::AABBd
+  include Rosegold::AABB(Float64)
+
+  property min : Vec3d, max : Vec3d
+
+  def initialize(@min : Vec3d, @max : Vec3d); end
+
+  def self.new(
+    min_x : Float64, min_y : Float64, min_z : Float64,
+    max_x : Float64, max_y : Float64, max_z : Float64
+  )
+    self.new(Vec3d.new(min_x, min_y, min_z), Vec3d.new(max_x, max_y, max_z))
+  end
 end
--- src/rosegold/world/player.cr
+++ src/rosegold/world/player.cr
@@ -2,6 +2,8 @@ require "./look"
 require "./vec3"
 
 class Rosegold::Player
+  PLAYER_AABB = AABBf.new -0.3, 0, -0.3, 0.3, 1.8, 0.3
+
   property \
     feet : Vec3d = Vec3d::ORIGIN,
     look : LookDeg = LookDeg::SOUTH,
@@ -10,4 +12,8 @@ class Rosegold::Player
     food : Float32 = 0,
     saturation : Float32 = 0,
     gamemode : UInt8 = 0
+
+  def aabb
+    PLAYER_AABB + feet
+  end
 end
