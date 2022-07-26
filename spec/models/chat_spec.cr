4d9e8f31c73ebdab384747783e0b7fff8198dc2d
Alex Piechowski <alex@piechowski.io>
2022-07-25 20:34:57 -0500

    Add chat translations

--- spec/models/chat_spec.cr
+++ spec/models/chat_spec.cr
@@ -7,5 +7,14 @@ Spectator.describe Rosegold::Chat do
     JSON
     expect(chat.color).to eq("gold")
     expect(chat.text).to eq("Connecting to the server...")
+    expect(chat.to_s).to eq("Connecting to the server...")
+  end
+
+  it "should translate" do
+    chat = Rosegold::Chat.from_json <<-JSON
+    {"translate":"chat.type.text","with":[{"insertion":"rosegold","clickEvent":{"action":"suggest_command","value":"/tell rosegold "},"hoverEvent":{"action":"show_entity","contents":{"type":"minecraft:player","id":"228ba5dc-a03c-3df0-8c5c-3385230406f0","name":{"text":"rosegold"}}},"text":"rosegold"},"Test chat message... Hello, Rosegold!"]}
+    JSON
+    expect(chat.translate).to eq("chat.type.text")
+    expect(chat.to_s).to eq("<rosegold> Test chat message... Hello, Rosegold!")
   end
 end
