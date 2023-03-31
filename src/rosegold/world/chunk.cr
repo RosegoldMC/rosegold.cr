142b715b4782d06f608530594bc16c6a027da9cf
Gjum <code.gjum@gmail.com>
2023-03-31 16:24:36 +0200

    chunk stores block_entities and light_data

--- src/rosegold/world/chunk.cr
+++ src/rosegold/world/chunk.cr
@@ -5,6 +5,8 @@ class Rosegold::Chunk
   alias BlockStateNr = UInt16
 
   getter sections : Array(Section)
+  property block_entities = Array(BlockEntity).new
+  property light_data : Bytes?
   private getter min_y : Int32
 
   def initialize(io, dimension : Dimension)
@@ -46,5 +48,13 @@ class Rosegold::Chunk
     def set_block_state(index : UInt32, block_state : BlockStateNr)
       @blocks[index] = block_state
     end
+  end
+
+  struct BlockEntity
+    property x : Int32, y : Int32, z : Int32
+    property type : UInt32
+    property nbt : Minecraft::NBT::Tag
+
+    def initialize(@x, @y, @z, @type, @nbt); end
   end
 end
