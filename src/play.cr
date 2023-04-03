8d4d4fdbda73ef9da04017969d3658418eb2741f
nebula <38367311+nebula161@users.noreply.github.com>
2023-04-02 20:19:44 -0400

    Push some testing stuff

--- src/play.cr
+++ src/play.cr
@@ -1,5 +1,8 @@
 require "./rosegold"
+require "./rosegold/bot"
 require "./microsoft/mobile_oauth"
+
+include Rosegold 
 
 def show_help
   puts "Rosegold v#{Rosegold::VERSION}"
@@ -9,56 +12,63 @@ def show_help
 end
 
 def startClient
-  Rosegold::Client.new("icenia-creative.mcnetwork.me", 25565).start do |bot|
-    show_help
+  
 
-    spawn do
-      loop do
-        bot.move_to rand(-10..10), -60, rand(-10..10)
-        sleep 3
-      end
-    end
+Client.new("localhost").join_game do |client|
+  bot = Rosegold::Bot.new client
 
+  show_help
+
+  spawn do
     loop do
-      gets.try do |input|
-        next if input.empty?
-
-        if input.starts_with? "\\"
-          command = input.split(" ")
-          case command.first
-          when "\\help"
-            show_help
-          when "\\position"
-            puts bot.player.feet
-          when "\\pitch"
-            if command.size > 1
-              bot.pitch = command[1].to_f
-            else
-              puts bot.pitch
-            end
-          when "\\yaw"
-            if command.size > 1
-              bot.yaw = command[1].to_f
-            else
-              puts bot.yaw
-            end
-          when "\\move"
-            bot.move_to command[1].to_f, command[2].to_f, command[3].to_f
-          when "\\jump"
-            bot.jump
-          when "\\debug"
-            Log.setup :debug
-          when "\\trace"
-            Log.setup :trace
-          end
-
-          next
-        end
-
-        bot.chat input
-      end
+      bot.move_to rand(-10..10), rand(-10..10)
+      sleep 3
     end
   end
+
+  while input = gets
+    next if input.empty?
+
+    bot.chat input unless input.starts_with? "\\"
+
+    command = input.split(" ")
+    case command.first
+    when "\\help"
+      show_help
+    when "\\position"
+      puts bot.feet
+    when "\\pitch"
+      if command.size > 1
+        bot.look &.with_pitch_deg command[1].to_f.to_f32
+      else
+        puts bot.look.pitch_deg
+      end
+    when "\\yaw"
+      if command.size > 1
+        bot.look &.with_yaw_deg command[1].to_f.to_f32
+      else
+        puts bot.look.yaw_deg
+      end
+    when "\\move"
+      spawn do
+        location = Vec3d.new command[1].to_f, bot.feet.y, command[3].to_f
+        begin
+          bot.move_to location
+          puts "Arrived at #{bot.feet}"
+        rescue ex
+          puts "Movement to #{location} failed:"
+          puts ex
+        end
+      end
+    when "\\jump"
+      bot.start_jump
+    when "\\debug"
+      Log.setup :debug
+    when "\\trace"
+      Log.setup :trace
+    end
+  end
+end
 end
 
 ticket = Microsoft::MobileOAuth.prompt_for_login!
