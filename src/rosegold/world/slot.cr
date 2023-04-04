558eb1e44c881e9f1077e9c1c076b937ab916fc7
Gjum <code.gjum@gmail.com>
2023-04-04 09:39:59 +0200

    implement Slot#write

--- src/rosegold/world/slot.cr
+++ src/rosegold/world/slot.cr
@@ -3,23 +3,35 @@ require "../../minecraft/nbt"
 struct Rosegold::Slot
   property item_id : UInt32
   property count : UInt8
-  property nbt : Minecraft::NBT::Tag?
+  property nbt : Minecraft::NBT::CompoundTag?
 
   def initialize(@item_id = 0, @count = 0, @nbt = nil); end
 
-  def self.read(io : IO) : Rosegold::Slot
+  def self.read(io) : Rosegold::Slot
     present = io.read_bool
     return new unless present # Empty slot
 
     item_id = io.read_var_int
     count = io.read_byte
     nbt = io.read_nbt
-    nbt = nil if nbt.is_a? Minecraft::NBT::EndTag
+    nbt = nil unless nbt.is_a? Minecraft::NBT::CompoundTag
 
     new(item_id, count, nbt)
   end
 
+  def write(io)
+    io.write present?
+    return unless present?
+    io.write item_id
+    io.write_byte count
+    io.write nbt || Minecraft::NBT::EndTag.new
+  end
+
   def empty?
     item_id <= 0 || count <= 0
+  end
+
+  def present?
+    !empty?
   end
 end
