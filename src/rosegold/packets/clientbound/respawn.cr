d2c2c5202126cf9540d27daead837ee51d17880b
Gjum <code.gjum@gmail.com>
2023-04-04 21:01:01 +0200

    Write Clientbound::Respawn

--- src/rosegold/packets/clientbound/respawn.cr
+++ src/rosegold/packets/clientbound/respawn.cr
@@ -8,13 +8,13 @@ class Rosegold::Clientbound::Respawn < Rosegold::Clientbound::Packet
     dimension_name : String,
     hashed_seed : Int64,
     gamemode : Int8,
-    prev_gamemode : Int8
+    previous_gamemode : Int8
   property? \
     is_debug : Bool,
     is_flat : Bool,
     copy_metadata : Bool
 
-  def initialize(@dimension, @dimension_name, @hashed_seed, @gamemode, @prev_gamemode, @is_debug, @is_flat, @copy_metadata); end
+  def initialize(@dimension, @dimension_name, @hashed_seed, @gamemode, @previous_gamemode, @is_debug, @is_flat, @copy_metadata); end
 
   def self.read(packet)
     self.new(
@@ -27,6 +27,20 @@ class Rosegold::Clientbound::Respawn < Rosegold::Clientbound::Packet
       packet.read_bool,
       packet.read_bool
     )
+  end
+
+  def write : Bytes
+    Minecraft::IO::Memory.new.tap do |buffer|
+      buffer.write @@packet_id
+      buffer.write dimension
+      buffer.write dimension_name
+      buffer.write_full hashed_seed
+      buffer.write gamemode
+      buffer.write previous_gamemode
+      buffer.write is_debug?
+      buffer.write is_flat?
+      buffer.write copy_metadata?
+    end.to_slice
   end
 
   def callback(client)
