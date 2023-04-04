d616f7af8bf8ab9ea75087fa0e2b1517959be4b5
Gjum <Gjum@users.noreply.github.com>
2023-04-05 00:29:42 +0200

    More packets (#76)
    * look yaw/pitch defaults to degrees
    
    * implement SpawnPlayer, EntityTeleport packets
    
    * implement Slot#write
    
    * Write EntityEquipment
    
    ---------
    
    Co-authored-by: Alex Piechowski <alex@piechowski.io>

--- src/rosegold/world/slot.cr
+++ src/rosegold/world/slot.cr
@@ -3,24 +3,36 @@ require "../../minecraft/nbt"
 struct Rosegold::Slot
   property item_id_int : UInt32
   property count : UInt8
-  property nbt : Minecraft::NBT::Tag?
+  property nbt : Minecraft::NBT::CompoundTag?
 
   def initialize(@item_id_int = 0, @count = 0, @nbt = nil); end
 
-  def self.read(io : IO) : Rosegold::Slot
+  def self.read(io) : Rosegold::Slot
     present = io.read_bool
     return new unless present # Empty slot
 
     item_id_int = io.read_var_int
     count = io.read_byte
     nbt = io.read_nbt
-    nbt = nil if nbt.is_a? Minecraft::NBT::EndTag
+    nbt = nil unless nbt.is_a? Minecraft::NBT::CompoundTag
 
     new(item_id_int, count, nbt)
   end
 
+  def write(io)
+    io.write present?
+    return unless present?
+    io.write item_id
+    io.write_byte count
+    io.write nbt || Minecraft::NBT::EndTag.new
+  end
+
   def empty?
     item_id <= 0 || count <= 0
+  end
+
+  def present?
+    !empty?
   end
 
   # Use to get the item_id in new-age string format
