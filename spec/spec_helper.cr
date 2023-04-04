97a8368b79a025740e13a97a4f5ab527b57f438b
Alex Piechowski <alex@piechowski.io>
2023-04-04 02:08:17 -0600

    Fix test suite "You have logged in from another location"
    Use 9 different accounts and cycle between them
    Resolves #72

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,10 @@
 require "../src/rosegold"
 require "spectator"
+
+test_count = 0
+Spectator.configure do |config|
+  config.before_each do |_| # Runs a block of code before each example.
+    test_count += 1
+    ENV["MC_NAME"] = "rosegoldtest#{(test_count % 9) + 1}"
+  end
+end
