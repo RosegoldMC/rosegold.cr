29871e6533912f5caf27ec949c18a02502533fe9
Gjum <code.gjum@gmail.com>
2023-04-13 03:36:29 +0200

    Window helpers

--- src/rosegold/control/inventory.cr
+++ src/rosegold/control/inventory.cr
@@ -6,6 +6,21 @@ class Rosegold::Inventory
 
   forward_missing_to @client.window
 
+  # Returns the number of matching items in the entire window, or in the given slots range.
+  #
+  # Example:
+  #   inventory.count "diamond_pickaxe" # => 2
+  #   inventory.count &.empty? # => 2
+  #   inventory.count { |slot| slot.item_id == "diamond_pickaxe" && slot.efficiency >= 4 } # => 1
+  #   inventory.count &.empty?, hotbar # => 1
+  def count(spec, slots = slots)
+    slots.select(&.matches? spec).sum(&.count)
+  end
+
+  def count(&spec : WindowSlot -> _)
+    count(spec)
+  end
+
   # Picks a matching slot, if it exists in the inventory.
   # Returns true if the item was picked, false otherwise.
   #
@@ -39,6 +54,48 @@ class Rosegold::Inventory
     pick(spec) || raise ItemNotFoundError.new("Item #{item_id} not found in inventory")
   end
 
-  class ItemNotFoundError < Exception
+  # Tries to transfer at least `count` matching items from the player inventory to the container, using shift-clicking.
+  # Returns the number of actually transferred items.
+  #
+  # Example:
+  #   inventory.withdraw_at_least 5, "diamond_pickaxe" # => 3
+  #   inventory.withdraw_at_least 5, &.empty?, hotbar # => 1
+  #   inventory.withdraw_at_least 5, { |slot| slot.item_id == "diamond_pickaxe" && slot.efficiency >= 4 } # => 2
+  def withdraw_at_least(count, spec, source : Array(WindowSlot) = content)
+    shift_click_at_least count, spec, source
   end
+
+  def withdraw_at_least(count, &spec : WindowSlot -> _)
+    withdraw_at_least(count, spec)
+  end
+
+  # Tries to transfer at least `count` matching items from the container to the player inventory, using shift-clicking.
+  # Returns the number of actually transferred items.
+  #
+  # Example:
+  #   inventory.deposit_at_least 5, "diamond_pickaxe" # => 3
+  #   inventory.deposit_at_least 5, &.empty?, hotbar # => 1
+  #   inventory.deposit_at_least 5, { |slot| slot.item_id == "diamond_pickaxe" && slot.efficiency >= 4 } # => 2
+  def deposit_at_least(count, spec, source : Array(WindowSlot) = inventory + hotbar)
+    shift_click_at_least count, spec, source
+  end
+
+  def deposit_at_least(count, &spec : WindowSlot -> _)
+    deposit_at_least(count, spec)
+  end
+
+  private def shift_click_at_least(count, spec, slots : Array(WindowSlot))
+    transferred = 0
+    # prefer large stacks for minimum clicks
+    # for equal stacks, preserve order
+    slots.sort_by { |s| -s.count }.each do |slot|
+      next unless slot.matches? spec
+      transferred += slot.count
+      click slot, shift: true
+      break if transferred >= count
+    end
+    transferred
+  end
+
+  class ItemNotFoundError < Exception; end
 end
