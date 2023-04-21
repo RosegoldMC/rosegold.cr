371e6279103a7b188939215c2f4ad94339cdff5b
Alex Piechowski <alex@piechowski.io>
2023-04-21 09:54:58 -0600

    Fix format
    RIP

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -5,7 +5,6 @@ def client
   Rosegold::Client.new("localhost", 25565, offline: {uuid: "00000000-0000-0000-0000-000000000000", username: "rosegoldtest1"})
 end
 
-test_count = 0
 Spectator.configure do |config|
   config.formatter = Spectator::Formatting::DocumentFormatter.new
 end
