3cf308bba072559bf96466e8f3f02335292cb3e1
drekamor <121772241+drekamor@users.noreply.github.com>
2023-05-16 22:05:00 +0100

    chunk_data_spec (#138)
    * clean up after tests
    
    * Format and refactor chat_message packet spec
    
    * methods to compare Compound Tags and LongArrayTags
    
    * add chunk_data_spec
    
    * fix formatting
    
    ---------
    
    Co-authored-by: Alex Piechowski <alex@piechowski.io>

--- src/minecraft/nbt.cr
+++ src/minecraft/nbt.cr
@@ -398,6 +398,10 @@ module Minecraft::NBT
     def inspect(io)
       io << @value.inspect
     end
+
+    def ==(other)
+      other.is_a?(CompoundTag) && self.value == other.value
+    end
   end
 
   class IntArrayTag < Tag
@@ -509,6 +513,10 @@ module Minecraft::NBT
 
     def inspect(io)
       io << "NBT[#{values.inspect} : LongArray]"
+    end
+
+    def ==(other)
+      other.is_a?(LongArrayTag) && self.values == other.values
     end
   end
 end
