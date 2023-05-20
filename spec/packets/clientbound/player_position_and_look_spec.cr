9d1f9a168aa0ccc3304f80e43e7c0da8d665e139
Drekamor <ddrekamor@gmail.com>
2023-05-20 10:55:34 +0100

    add player_position_and_look spec

---
+++ spec/packets/clientbound/player_position_and_look_spec.cr
@@ -0,0 +1,34 @@
+require "../../spec_helper"
+
+Spectator.describe Rosegold::Clientbound::PlayerPositionAndLook do
+  let(:io) { Minecraft::IO::Memory.new(File.read(file)) }
+  let(:file) { File.expand_path("../../../fixtures/packets/clientbound/player_position_and_look.mcpacket", __FILE__) }
+  let(:file_slice) { File.read(file).to_slice }
+
+  it "parses the packet" do
+    io.read_byte
+    packet = Rosegold::Clientbound::PlayerPositionAndLook.read(io)
+
+    expect(packet.x_raw).to be_a(Float64)
+    expect(packet.y_raw).to be_a(Float64)
+    expect(packet.z_raw).to be_a(Float64)
+    expect(packet.yaw_raw).to be_a(Float32)
+    expect(packet.pitch_raw).to be_a(Float32)
+    expect(packet.relative_flags).to be_a(UInt8)
+    expect(packet.teleport_id).to be_a(UInt32)
+
+    expect(packet.x_raw).to eq(-5010.449320949349)
+    expect(packet.y_raw).to eq(-53.0)
+    expect(packet.z_raw).to eq(704.3544759367975)
+    expect(packet.yaw_raw).to eq(Float32.new(-88.24123))
+    expect(packet.pitch_raw).to eq(Float32.new(-29.699776))
+    expect(packet.relative_flags).to eq(0)
+    expect(packet.teleport_id).to eq(2)
+  end
+
+  it "writes packet the same after parsing" do
+    io.read_byte
+
+    expect(Rosegold::Clientbound::PlayerPositionAndLook.read(io).write).to eq file_slice
+  end
+end
