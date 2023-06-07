630ef8bd7588c53a2ec230acd6c1a8d901051daa
Alex Piechowski <alex@piechowski.io>
2023-06-07 18:38:34 +0300

    Format effect files

--- src/rosegold/models/effect.cr
+++ src/rosegold/models/effect.cr
@@ -1,25 +1,51 @@
 module Rosegold
   enum Effect
     Unused
-    Speed; Slowness; Haste
-    MiningFatigue; Strength; InstantHealth
-    InstantDamage; JumpBoost; Nausea
-    Regeneration; Resistance; FireResistance
-    WaterBreathing; Invisibility; Blindness
-    NightVision; Hunger; Weakness
-    Poison; Wither; HealthBoost
-    Absorption; Saturation; Glowing
-    Levitation; Luck; Unluck
-    SlowFalling; ConduitPower; DolphinsGrace
-    BadOmen; HeroOfTheVillage; Darkness
+    Speed
+    Slowness
+    Haste
+    MiningFatigue
+    Strength
+    InstantHealth
+    InstantDamage
+    JumpBoost
+    Nausea
+    Regeneration
+    Resistance
+    FireResistance
+    WaterBreathing
+    Invisibility
+    Blindness
+    NightVision
+    Hunger
+    Weakness
+    Poison
+    Wither
+    HealthBoost
+    Absorption
+    Saturation
+    Glowing
+    Levitation
+    Luck
+    Unluck
+    SlowFalling
+    ConduitPower
+    DolphinsGrace
+    BadOmen
+    HeroOfTheVillage
+    Darkness
 
     def display_name
-      return "Dolphin's Grase" if self == DolphinsGrace
+      return "Dolphin's Grace" if self == DolphinsGrace
+
       self.to_s.split(/(?=[A-Z])/).join(' ')
     end
 
     def name
-      self.to_s.split(/(?=[A-Z])/).map { |word| word.downcase }.join('_')
+      self
+        .to_s
+        .split(/(?=[A-Z])/)
+        .map(&.downcase).join('_')
     end
   end
 end
