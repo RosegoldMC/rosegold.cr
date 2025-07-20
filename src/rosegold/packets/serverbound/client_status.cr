38f38c6bdb552a64589844f725200ba730894276
Alex Piechowski <alex@piechowski.io>
2025-07-20 15:00:59 -0700

    Fix more packet id

--- src/rosegold/packets/serverbound/client_status.cr
+++ src/rosegold/packets/serverbound/client_status.cr
@@ -5,8 +5,8 @@ class Rosegold::Serverbound::ClientStatus < Rosegold::Serverbound::Packet
   # Define protocol-specific packet IDs
   packet_ids({
     758_u32 => 0x04_u8, # MC 1.18
-    767_u32 => 0x09_u8, # MC 1.21
-    771_u32 => 0x09_u8, # MC 1.21.6
+    767_u32 => 0x0B_u8, # MC 1.21
+    771_u32 => 0x0B_u8, # MC 1.21.6
   })
 
   enum Action
