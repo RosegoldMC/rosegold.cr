20041e54d8cbfb516220f2a86d66219c6bbc9840
Alex Piechowski <alex@piechowski.io>
2025-07-21 13:06:14 -0700

    Fix packet ids

--- src/rosegold/packets/clientbound/block_change.cr
+++ src/rosegold/packets/clientbound/block_change.cr
@@ -5,8 +5,8 @@ class Rosegold::Clientbound::BlockChange < Rosegold::Clientbound::Packet
   # Define protocol-specific packet IDs
   packet_ids({
     758_u32 => 0x0c_u8, # MC 1.18
-    767_u32 => 0x09_u8, # MC 1.21
-    771_u32 => 0x09_u8, # MC 1.21.6
+    767_u32 => 0x08_u8, # MC 1.21
+    771_u32 => 0x08_u8, # MC 1.21.6
   })
 
   property \
