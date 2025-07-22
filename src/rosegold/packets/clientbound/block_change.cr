256925b0b8dd8f3f38913e2636a9ce0cace745ff
Alex Piechowski <alex@piechowski.io>
2025-07-21 20:35:04 -0700

    Fix movement specs for 1.21.4

--- src/rosegold/packets/clientbound/block_change.cr
+++ src/rosegold/packets/clientbound/block_change.cr
@@ -5,8 +5,8 @@ class Rosegold::Clientbound::BlockChange < Rosegold::Clientbound::Packet
   # Define protocol-specific packet IDs
   packet_ids({
     758_u32 => 0x0c_u8, # MC 1.18
-    767_u32 => 0x08_u8, # MC 1.21
-    769_u32 => 0x08_u8, # MC 1.21.4,
+    767_u32 => 0x09_u8, # MC 1.21
+    769_u32 => 0x09_u8, # MC 1.21.4,
     771_u32 => 0x08_u8, # MC 1.21.6,
   })
 
