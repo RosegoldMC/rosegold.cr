2c801a59451264fd768a07b2b80fc2b9a7b281ee
Alex Piechowski <alex@piechowski.io>
2025-08-14 22:22:07 -0700

    Allow server to put state back to reconfiguration
    pause physics for that time too

---
+++ src/rosegold/packets/serverbound/acknowledge_configuration.cr
@@ -0,0 +1,23 @@
+require "../packet"
+
+class Rosegold::Serverbound::AcknowledgeConfiguration < Rosegold::Serverbound::Packet
+  include Rosegold::Packets::ProtocolMapping
+
+  packet_ids({
+    772_u32 => 0x0F_u8, # MC 1.21.8,
+  })
+
+  class_getter state = ProtocolState::PLAY
+
+  def initialize; end
+
+  def self.read(packet)
+    self.new
+  end
+
+  def write : Bytes
+    Minecraft::IO::Memory.new.tap do |buffer|
+      buffer.write self.class.packet_id_for_protocol(Client.protocol_version)
+    end.to_slice
+  end
+end
