5d2c7bcf42928f6dcee75291b712fe559ccb8561
Alex Piechowski <alex@piechowski.io>
2025-08-21 16:12:14 -0700

    Fix format

--- spec/integration/bed_spec.cr
+++ spec/integration/bed_spec.cr
@@ -20,7 +20,7 @@ Spectator.describe "Rosegold::Bot bed interactions" do
         # Set time to night to allow sleeping
         bot.chat "/time set night"
         bot.wait_tick
-        
+
         # Place a bed at a specific location
         bot.chat "/setblock 0 -60 0 minecraft:red_bed[part=foot,facing=north]"
         bot.chat "/setblock 0 -60 1 minecraft:red_bed[part=head,facing=north]"
@@ -29,25 +29,19 @@ Spectator.describe "Rosegold::Bot bed interactions" do
         # Teleport the bot directly to the bed position
         bot.chat "/tp 0.5 -60 0.5"
         bot.wait_tick
-        
-        # Record initial position
-        initial_position = bot.feet
 
         # Interact with the bed to get in (look down at the bed)
         bot.look_at Rosegold::Vec3d.new(0.5, -60.5, 0.5)
         bot.use_hand
         bot.wait_ticks 5 # Wait for bed interaction to process
 
-        # Position may have changed when sleeping
-        sleeping_position = bot.feet
-        
         # Get out of bed using the leave_bed method
         bot.leave_bed
         bot.wait_ticks 3
 
         # Position should be valid after leaving bed
         final_position = bot.feet
-        
+
         # Bot should still be functional after bed interaction
         expect(client.connected?).to be_true
         expect(final_position).to be_a(Rosegold::Vec3d)
@@ -61,7 +55,7 @@ Spectator.describe "Rosegold::Bot bed interactions" do
         # Set time to day
         bot.chat "/time set day"
         bot.wait_tick
-        
+
         # Place a bed
         bot.chat "/setblock 2 -60 0 minecraft:blue_bed[part=foot,facing=east]"
         bot.chat "/setblock 3 -60 0 minecraft:blue_bed[part=head,facing=east]"
@@ -70,7 +64,7 @@ Spectator.describe "Rosegold::Bot bed interactions" do
         # Teleport the bot to the bed
         bot.chat "/tp 2.5 -60 0.5"
         bot.wait_tick
-        
+
         # Try to interact with the bed during day
         bot.look_at Rosegold::Vec3d.new(2.5, -60.5, 0.5)
         bot.use_hand
@@ -81,7 +75,7 @@ Spectator.describe "Rosegold::Bot bed interactions" do
         # We'll just verify the bot is still functional
         expect(bot).to be_truthy
         expect(client.connected?).to be_true
-        
+
         # Try to leave bed anyway (should not cause issues even if not sleeping)
         bot.leave_bed
         bot.wait_ticks 2
@@ -96,7 +90,7 @@ Spectator.describe "Rosegold::Bot bed interactions" do
         # Set time to night to allow sleeping
         bot.chat "/time set night"
         bot.wait_tick
-        
+
         # Place a bed
         bot.chat "/setblock 4 -60 0 minecraft:green_bed[part=foot,facing=west]"
         bot.chat "/setblock 3 -60 0 minecraft:green_bed[part=head,facing=west]"
@@ -105,22 +99,16 @@ Spectator.describe "Rosegold::Bot bed interactions" do
         # Teleport the bot to the bed
         bot.chat "/tp 3.5 -60 0.5"
         bot.wait_tick
-        
-        # Record initial position
-        initial_position = bot.feet
-        
+
         # Interact with the bed at night
         bot.look_at Rosegold::Vec3d.new(3.5, -60.5, 0.5)
         bot.use_hand
         bot.wait_ticks 5 # Wait for bed interaction to process
 
-        # Position may have changed if sleeping was successful
-        sleeping_position = bot.feet
-        
         # Wake up using leave_bed method
         bot.leave_bed
         bot.wait_ticks 3
-        
+
         # Verify bot is still functional
         expect(client.connected?).to be_true
         final_position = bot.feet
