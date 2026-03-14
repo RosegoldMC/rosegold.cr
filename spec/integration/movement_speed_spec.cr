d1d3c55689d3b525e4a4719c18e5d9e84486cefe
Alex Piechowski <alex@piechowski.io>
2026-03-14 08:47:46 +0700

    Fix linter warnings and deprecations
    Replace Time.monotonic with Time.instant to resolve
    Crystal deprecation warnings. Fix ameba lint issues:
    replace not_nil! with proper type casts, prefix unused
    block argument, and use short block notation.

--- spec/integration/movement_speed_spec.cr
+++ spec/integration/movement_speed_spec.cr
@@ -19,27 +19,27 @@ Spectator.describe "Rosegold::Bot movement speeds" do
         bot.wait_tick
 
         bot.sneak(true)
-        start_time = Time.monotonic
+        start_time = Time.instant
         bot.move_to 5, 0
-        sneak_time = Time.monotonic - start_time
+        sneak_time = Time.instant - start_time
         sneak_speed = 5.0 / sneak_time.total_seconds
 
         bot.chat "/tp 0 -60 0"
         bot.wait_tick
         bot.sneak(false)
 
-        start_time = Time.monotonic
+        start_time = Time.instant
         bot.move_to 5, 0
-        walk_time = Time.monotonic - start_time
+        walk_time = Time.instant - start_time
         walk_speed = 5.0 / walk_time.total_seconds
 
         bot.chat "/tp 0 -60 0"
         bot.wait_tick
 
         bot.sprint(true)
-        start_time = Time.monotonic
+        start_time = Time.instant
         bot.move_to 5, 0
-        sprint_time = Time.monotonic - start_time
+        sprint_time = Time.instant - start_time
         sprint_speed = 5.0 / sprint_time.total_seconds
 
         expect(sneak_speed).to be_close(walk_speed * 0.3, walk_speed * 0.1)
