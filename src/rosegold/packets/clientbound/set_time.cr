3019a1ad19dfcb75467fbfd7d8aeddcb4724ced0
Alex Piechowski <alex@piechowski.io>
2026-03-22 12:20:11 +0700

    Fix ameba warning in SetTime packet
    Use property? for Bool field tick_day_time per Crystal
    naming convention for boolean accessors.

--- src/rosegold/packets/clientbound/set_time.cr
+++ src/rosegold/packets/clientbound/set_time.cr
@@ -10,7 +10,7 @@ class Rosegold::Clientbound::SetTime < Rosegold::Clientbound::Packet
 
   property world_age : Int64
   property time_of_day : Int64
-  property tick_day_time : Bool
+  property? tick_day_time : Bool
 
   def initialize(@world_age, @time_of_day, @tick_day_time = true); end
 
@@ -26,13 +26,13 @@ class Rosegold::Clientbound::SetTime < Rosegold::Clientbound::Packet
       buffer.write self.class.packet_id_for_protocol(Client.protocol_version)
       buffer.write_full world_age
       buffer.write_full time_of_day
-      buffer.write tick_day_time
+      buffer.write tick_day_time?
     end.to_slice
   end
 
   def callback(client)
     client.dimension.time_of_day = time_of_day
     client.dimension.world_age = world_age
-    Log.debug { "SetTime: world_age=#{world_age}, time_of_day=#{time_of_day}, tick_day_time=#{tick_day_time}" }
+    Log.debug { "SetTime: world_age=#{world_age}, time_of_day=#{time_of_day}, tick_day_time=#{tick_day_time?}" }
   end
 end
